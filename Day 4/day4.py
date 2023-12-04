cef3ff7048b7c5de577e19feeddeb36e0ed365f4
Malik Müller <malik.mueller@ruhr-uni-bochum.de>
2023-12-04 21:50:10 +0100

    Day 4 cleaned

--- Day 4/day4.py
+++ Day 4/day4.py
@@ -22,13 +22,11 @@ for i in range(0, len(data)):
     elif counter == 1:
         win_sum += 1
 print("Part 1: " + str(win_sum))
-    
 
-queue = [1]
+# Part 2
 cards = {}
 for i in range(0, len(data)):
     cards[i+1] = 1
-# Part 2
 i = 1
 for i in range(1, len(data)):
     counter = 0
@@ -42,12 +40,6 @@ for i in range(1, len(data)):
     for x in numbers_int:
         if x in winning_numbers_int:
             counter += 1
-    # game is the number of the card
-    # for each win in counter, add 1 to each following card.
-    # for example game 1 wins 3, then game 2 += 1, game 3 += 1, game 4 += 1
-    # if game 2 wins 2, then game 3 += 1, game 4 += 1
-    # add these to the queue
-    # remove the current from the queue
 
     if counter > 1:
         for x in range(1, counter+1):
@@ -56,5 +48,4 @@ for i in range(1, len(data)):
         cards[game+1] += cards[game]
 
 # print the sum of the values in the dictionary
-print(cards)
 print("Part 2: " + str(sum(cards.values())))    
